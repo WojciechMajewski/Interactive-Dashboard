5d7620f3a5ddfd5ce1164afddd7ace562e07d7ef
TwistedTaleTeller <wojciechmajewski330@gmail.com>
2022-05-31 03:15:44 +0200

    progress

--- InteractiveDashboard/ui.R
+++ InteractiveDashboard/ui.R
@@ -8,26 +8,14 @@
 #
 
 library(shiny)
+library(DT)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
     titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+    
+    
+    DT::dataTableOutput("mytable", height = 800, width = 600)
 ))
